ad3e8f1d56253e4daa7ecee850a3373af4f314bb
prmr <prmr@gmx.com>
2019-04-26 10:27:16 -0400

    Finalize TwelveDays

--- chapter-code/chapter01/TwelveDays.java
+++ chapter-code/chapter01/TwelveDays.java
@@ -1,10 +1,26 @@
+/*******************************************************************************
+ * Companion code for the book "Introduction to Software Design with Java" 
+ * by Martin P. Robillard.
+ *
+ * Copyright (C) 2019 by Martin P. Robillard
+ *
+ * This code is licensed under a Creative Commons 
+ * Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * 
+ * See http://creativecommons.org/licenses/by-nc-nd/4.0/
+ *******************************************************************************/
 package chapter01;
 
+/**
+ * Outputs the text of the poem "The Twelve Days of Christmas"
+ * to the console. The code leverages the natural recursion in 
+ * the structure of the poem.
+ */
 public class TwelveDays
 {
 	public static void main(String[] args)
 	{
-		System.out.println(song());
+		System.out.println(poem());
 	}
 	
 	static String[] DAYS = {"first", "second", "third", "fourth",
@@ -26,20 +42,41 @@ public class TwelveDays
 			"twelve Drummers Drumming"
 	};
 	
-	static String intro(int day) 
-	{ return "On the " + DAYS[day] + " day of Christmas\nmy true love sent to me:\n"; }
-	
-	static String allGifts(int day)
-	{
-		if( day == 0 ) { return "and " + GIFTS[0]; }
-		else { return GIFTS[day] + "\n" + allGifts(day-1);	}
+	/*
+     * Returns the first verse in the stanza for a given day.
+	 */
+	static String firstVerse(int day) 
+	{ 
+		return "On the " + DAYS[day] + 
+				" day of Christmas\nmy true love sent to me:\n"; 
 	}
 	
-	static String song()
+	/*
+     * Returns a string that lists all the gifts received on a given
+     * day.
+	 */
+	static String allGifts(int day)
 	{
-		String song = intro(0) + GIFTS[0] + "\n\n";
+		if( day == 0 ) 
+		{ 
+			return "and " + GIFTS[0]; 
+		}
+		else 
+		{ 
+			return GIFTS[day] + "\n" + allGifts(day-1);	
+		}
+	}
+	
+	/*
+	 * Returns the text of the entire poem. 
+	 */
+	static String poem()
+	{
+		String poem = firstVerse(0) + GIFTS[0] + "\n\n";
 		for( int day = 1; day < 12; day++ )
-		{ song += intro(day) + allGifts(day) + "\n\n"; }
-		return song;
+		{ 
+			poem += firstVerse(day) + allGifts(day) + "\n\n"; 
+		}
+		return poem;
 	}
 }
