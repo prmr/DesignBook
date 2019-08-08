83586fee4b84ea096b2fdd44f3af6074c8db181b
prmr <prmr@gmx.com>
2019-08-08 16:00:16 -0400

    Add Chapter 05 Sample Code

---
+++ chapter-code/chapter05/Rank.java
@@ -0,0 +1,9 @@
+package chapter05;
+
+/**
+ * Represents the rank of a playing card.
+ */
+public enum Rank 
+{ 
+	ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING 
+}
---
+++ chapter-code/chapter05/Suit.java
@@ -0,0 +1,10 @@
+package chapter05;
+
+/**
+ * Represents the suit of a playing card.
+ */
+public enum Suit 
+{ 
+	CLUBS, DIAMONDS, SPADES, HEARTS;
+}
+
