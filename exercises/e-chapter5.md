80be88e48f11a8ed207363efeb7a8c6e861bd07f
prmr <prmr@gmx.com>
2019-10-10 15:31:37 -0400

    Finalize exercises for Chapter 5

--- exercises/e-chapter5.md
+++ exercises/e-chapter5.md
@@ -28,6 +28,18 @@ Using metaprogramming, verify whether it is possible to call any parameterless m
 
 Write unit test for methods `push` and `pop` of class [Stack](https://docs.oracle.com/javase/8/docs/api/java/util/Stack.html). Use a fixture that you initialize before every test, and make sure your tests cover documented exceptional behavior.
 
+## Exercise 8
+
+Class [PokerHand](../solutions-code/chapter5/PokerHand.java) is a partial solution to [exercise 3.9](e-chapter3.md). Write a test class `TestPokerHand` with helper methods than make it possible to test the private method `isStraightFlush`, then write a test case for which the output is `true`. For this method, how many executions of the UUT do we need to test it with *all possible straight flushes*? Does this mean we can achieve exhaustive test coverage with these test cases?
+
+## Exercise 9
+
+Extend your test suite for [PokerHand](../solutions-code/chapter5/PokerHand.java) to achieve *complete statement coverage* with a minimum number of executions of the UUT.
+
+## Exercise 10
+
+Extend your test suite for [PokerHand](../solutions-code/chapter5/PokerHand.java) to achieve *complete branch coverage* with a minimum number of executions of the UUT.
+
 ---
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a>
 
---
+++ solutions-code/chapter5/PokerHand.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Companion code for the book "Introduction to Software Design with Java" 
+ * by Martin P. Robillard.
+ *
+ * Copyright (C) 2019 by Martin P. Robillard
+ *
+ * This code is licensed under a Creative Commons 
+ * Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * 
+ * See http://creativecommons.org/licenses/by-nc-nd/4.0/
+ *******************************************************************************/
+package chapter5;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+// Let's assume we only consider ace-low as valid for straights, for simplicity.
+public class PokerHand
+{
+	private final List<Card> aCards;
+	
+	public PokerHand( Card... pCards)
+	{
+		assert pCards.length == 5;
+		aCards = Arrays.asList(pCards);
+		aCards.sort(new Comparator<Card>() {
+			@Override
+			public int compare(Card pCard1, Card pCard2)
+			{
+				return pCard1.getRank().compareTo(pCard2.getRank());
+			}});
+	}
+	
+	private boolean isStraightFlush()
+	{
+		Suit suit  = aCards.get(0).getSuit();
+		Rank rank = aCards.get(0).getRank();
+		Iterator<Card> iterator = aCards.iterator();
+		iterator.next();
+		while( iterator.hasNext() )
+		{
+			Card card = iterator.next();
+			if( card.getSuit() != suit || card.getRank().ordinal() - rank.ordinal() != 1)
+			{
+				return false;
+			}
+			rank = card.getRank();
+		}
+		return true;
+	}
+}
