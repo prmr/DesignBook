f95d8394ffaa859fdd4c0d269bba2482b3a87b07
prmr <prmr@gmx.com>
2019-11-25 08:36:06 -0500

    Update

--- solutions-code/chapter8/UserInterface.java
+++ solutions-code/chapter8/UserInterface.java
@@ -20,12 +20,8 @@ public class UserInterface extends Application
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{
-		Label label = new Label(LocalDateTime.now().toString());
-		Button button = new Button("Now");
-		button.setOnAction(e -> label.setText(LocalDateTime.now().toString()));
-		
 		Parent root = null; // Initialize with a component graph
-		primaryStage.setScene(new Scene(new HBox(label,button)));
+		primaryStage.setScene(new Scene(root));
 		primaryStage.show();
 	}
 
