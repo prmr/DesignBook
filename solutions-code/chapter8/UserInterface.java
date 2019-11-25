a6d577099bf08384ad0d87f845e2d842cf3a5e35
prmr <prmr@gmx.com>
2019-11-25 08:36:23 -0500

    Update

--- solutions-code/chapter8/UserInterface.java
+++ solutions-code/chapter8/UserInterface.java
@@ -1,13 +1,8 @@
 package chapter8;
 
-import java.time.LocalDateTime;
-
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class UserInterface extends Application
