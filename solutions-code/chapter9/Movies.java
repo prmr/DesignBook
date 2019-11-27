ad120dc9353a2a6d83c21e652c4a6f8ebb211624
prmr <prmr@gmx.com>
2019-11-27 09:36:46 -0500

    Switch selection

--- solutions-code/chapter9/Movies.java
+++ solutions-code/chapter9/Movies.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 public class Movies
 {
-	public static final Movie CARVE_UP = new Movie("What a Carve Up!", 1961, 87);
-	public static final Movie GIANT_CLAW = new Movie("The Giant Claw", 1957, 75);
-	public static final Movie ALLIGATOR = new Movie("The Alligator People", 1959, 75);
-	public static final Movie CREEPING = new Movie("The Creeping Terror", 1964, 77);
-	public static final Movie LEECHES = new Movie("Attack of the Giant Leeches", 1959, 62);
+	public static final Movie OLDBOY = new Movie("Oldboy", 2003, 121);
+	public static final Movie METROPOLIS = new Movie("Metropolis", 1927, 153);
+	public static final Movie WAGES = new Movie("The Wages of Fear", 1953, 131);
+	public static final Movie INTOUCHABLES = new Movie("The Intouchables", 2011, 112);
+	public static final Movie AMELIE = new Movie("Amelie", 2001, 121);
 	
 	public static final List<Movie> movies()
 	{
-		return Arrays.asList(CARVE_UP, GIANT_CLAW, ALLIGATOR, CREEPING, LEECHES);
+		return Arrays.asList(OLDBOY, METROPOLIS, WAGES, INTOUCHABLES, AMELIE);
 	}
 }
