1839622b8c1472d25aef913c3ecd09ba7cb62f48
prmr <prmr@gmx.com>
2019-11-29 14:04:13 -0500

    Add some movies to Chapter 9 exercises

--- solutions-code/chapter9/Movies.java
+++ solutions-code/chapter9/Movies.java
@@ -10,9 +10,13 @@ public class Movies
 	public static final Movie WAGES = new Movie("The Wages of Fear", 1953, 131);
 	public static final Movie INTOUCHABLES = new Movie("The Intouchables", 2011, 112);
 	public static final Movie AMELIE = new Movie("Amelie", 2001, 121);
+	public static final Movie GOODBAD = new Movie("The Good, the Bad, and the Ugly", 1966, 178);
+	public static final Movie DIABOLIQUE = new Movie("Diabolique", 1955, 117);
+	public static final Movie JOAN = new Movie("The Passion of Joan of Arc", 1928, 114);
+
 	
 	public static final List<Movie> movies()
 	{
-		return Arrays.asList(OLDBOY, METROPOLIS, WAGES, INTOUCHABLES, AMELIE);
+		return Arrays.asList(OLDBOY, METROPOLIS, WAGES, INTOUCHABLES, AMELIE, GOODBAD, DIABOLIQUE, JOAN);
 	}
 }
