eed9fe8b0022a4add7962560e8aabc17c43161e8
prmr <prmr@gmx.com>
2019-12-04 15:37:37 -0500

    Add header

--- solutions-code/chapter6/CardImages.java
+++ solutions-code/chapter6/CardImages.java
@@ -1,5 +1,15 @@
+/*******************************************************************************
+ * Companion code for the book "Introduction to Software Design with Java" 
+ * by Martin P. Robillard.
+ *
+ * Copyright (C) 2019 by Martin P. Robillard
+ *
+ * This code is licensed under a Creative Commons 
+ * Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * 
+ * See http://creativecommons.org/licenses/by-nc-nd/4.0/
+ *******************************************************************************/
 package chapter6;
-
 
 import java.util.HashMap;
 import java.util.Map;
