d1e5397727cb96e18cbb1be050a5cf0b4db9dede
prmr <prmr@gmx.com>
2020-11-08 16:57:42 -0500

    Modularize the application

---
+++ chapter-code/module-info.java
@@ -0,0 +1,18 @@
+module designbook
+{
+	exports chapter4;
+	exports chapter3;
+	exports chapter2;
+	exports chapter1;
+	exports chapter8;
+	exports chapter7;
+	exports chapter6;
+	exports chapter5;
+	exports chapter9;
+
+	requires java.desktop;
+	requires javafx.controls;
+	requires transitive javafx.graphics;	
+	requires static junit;
+	requires static org.junit.jupiter.api;
+}
