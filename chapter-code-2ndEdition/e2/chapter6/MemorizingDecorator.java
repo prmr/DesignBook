32db2d058abf07957f872c072a784b9916241733
Martin Robillard <prmr@gmx.com>
2022-05-06 11:58:33 -0400

    Add code examples for Chapter 6 of the 2nd edition.

---
+++ chapter-code-2ndEdition/e2/chapter6/MemorizingDecorator.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Companion code for the book "Introduction to Software Design with Java",
+ * 2nd edition by Martin P. Robillard.
+ *
+ * Copyright (C) 2022 by Martin P. Robillard
+ *
+ * This code is licensed under a Creative Commons 
+ * Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * 
+ * See http://creativecommons.org/licenses/by-nc-nd/4.0/
+ * 
+ *******************************************************************************/
+package e2.chapter6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A CardSource decorator that memorizes cards drawn from the
+ * source.
+ */
+public class MemorizingDecorator implements CardSource
+{
+	private CardSource aElement;
+	private List<Card> aDrawnCards = new ArrayList<>();
+
+	public MemorizingDecorator(CardSource pCardSource) {
+		aElement = pCardSource;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return aElement.isEmpty();
+	}
+
+	@Override
+	public Card draw() {
+		// 1. Delegate the original request to the decorated object
+		Card card = aElement.draw();
+		// 2. Implement the decoration
+		aDrawnCards.add(card);
+		return card;
+	}
+
+	@Override
+	public CardSource copy() {
+		MemorizingDecorator copy = new MemorizingDecorator(aElement.copy());
+		copy.aDrawnCards = new ArrayList<>(aDrawnCards);
+		return copy;
+	}
+}
