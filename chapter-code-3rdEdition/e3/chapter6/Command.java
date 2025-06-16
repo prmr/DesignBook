f793aa3683da8f13fa314dba368b320de01994b8
Martin Robillard <prmr@gmx.com>
2025-06-16 17:29:47 +0200

    Add chapter 6 code for 3rd edition

---
+++ chapter-code-3rdEdition/e3/chapter6/Command.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Companion code for the book "Introduction to Software Design with Java",
+ * 2nd edition by Martin P. Robillard.
+ *
+ * Copyright (C) 2025 by Martin P. Robillard
+ *
+ * This code is licensed under a Creative Commons 
+ * Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * 
+ * See http://creativecommons.org/licenses/by-nc-nd/4.0/
+ * 
+ *******************************************************************************/
+package e3.chapter6;
+
+/**
+ * Represents an abstract command in the Command design pattern.
+ */
+public interface Command
+{
+	void execute();
+	
+	void undo();
+}
---
+++ chapter-code-3rdEdition/e3/chapter6/LoggingDecorator.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Companion code for the book "Introduction to Software Design with Java",
+ * 2nd edition by Martin P. Robillard.
+ *
+ * Copyright (C) 2025 by Martin P. Robillard
+ *
+ * This code is licensed under a Creative Commons 
+ * Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * 
+ * See http://creativecommons.org/licenses/by-nc-nd/4.0/
+ * 
+ *******************************************************************************/
+package e3.chapter6;
+
+/**
+ * A CardSource decorator that logs cards drawn from the
+ * source.
+ */
+public class LoggingDecorator implements CardSource {
+	
+	private CardSource aSource;
+	
+	public LoggingDecorator( CardSource pSource ) {
+		aSource = pSource;
+	}
+
+	@Override
+	public Card draw() {
+		Card card = aSource.draw();
+		System.out.println(card);
+		return card;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return aSource.isEmpty();
+	}
+
+	@Override
+	public CardSource copy() {
+		return new LoggingDecorator(aSource.copy());
+	}
+}
