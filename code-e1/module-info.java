24b89b8ab5467755e25bc49bfbb8a84b0928f9ec
Martin Robillard <prmr@gmx.com>
2025-10-22 17:20:23 -0400

    Update module-info to support the 2nd edition

--- code-e1/module-info.java
+++ code-e1/module-info.java
@@ -1,14 +1,24 @@
 module designbook
 {
-	exports e1.chapter4;
-	exports e1.chapter3;
-	exports e1.chapter2;
 	exports e1.chapter1;
-	exports e1.chapter8;
-	exports e1.chapter7;
-	exports e1.chapter6;
+	exports e1.chapter2;
+	exports e1.chapter3;
+	exports e1.chapter4;
 	exports e1.chapter5;
+	exports e1.chapter6;
+	exports e1.chapter7;
+	exports e1.chapter8;
 	exports e1.chapter9;
+	
+	exports e2.chapter1;
+	exports e2.chapter2;
+	exports e2.chapter3;
+	exports e2.chapter4;
+	exports e2.chapter5;
+	exports e2.chapter6;
+	exports e2.chapter7;
+	exports e2.chapter8;
+	exports e2.chapter9;
 
 	requires java.desktop;
 	requires javafx.controls;
